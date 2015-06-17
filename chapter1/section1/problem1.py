07d4cc9ee394cee98e82d00e63052a8fcdee872a
csheahan <chris.sheahan@live.com>
2015-06-17 00:11:08 -0700

    Finish problem 1.1.1

---
+++ chapter1/section1/problem1.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+'''
+Probelem:
+  Given a number α, find rational number p/q such that |α - p/q| ≤ 1/q^2
+
+Constraints:
+  α - must be an integer
+  p - must be an integer
+  q - must be an integer s.t. q != 0
+
+Observation:
+  if p/q = α, then the inequality is guaranteed to hold. Leverage this
+  observation to not do costly and potentially wrong via rounding error
+  calculations
+'''
+
+###############################################################################
+# problem - finds a rational number p/q such that |α - p / q| ≤ 1/q^2
+#
+# @param int alpha - α parameter to use
+#
+# @return a list of tuples (p, q, lhs, rhs) that follows the inequality where
+#         p = p
+#         q = q
+#         lhs = |α - p/q|
+#         rhs = 1/q^2
+###
+def problem(alpha):
+  print alpha
+  answers = []
+
+  q = alpha
+  p = q * alpha
+  lhs = abs(alpha - float(p) / q)
+  rhs = 1 / (float(q) * q)
+  answers.append((p, q, lhs, rhs))
+
+  return answers
